4d580250d77f5b3cd7ae6d06c96eb30072f621a9
thebrandconsultant <james@thebrandconsultant.co.za>
2020-03-17 13:52:13 +0200

    Update what-we-do.js

--- src/templates/what-we-do.js
+++ src/templates/what-we-do.js
@@ -14,7 +14,7 @@ export const WhatWeDoPageTemplate = ({ title, content, contentComponent }) => {
           <div className="column is-10 is-offset-1">
             <div className="section">
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title} test
+                {title}
               </h2>
               <PageContent className="content" content={content} />
             </div>
