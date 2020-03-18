1446511e84fcfc07fd99e2ab630a36661f756b2a
thebrandconsultant <james@thebrandconsultant.co.za>
2020-03-18 17:55:17 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Centre for Analytics & Behavioural Change',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'The Centre for Analytics and Behavioural Change (CABC) was created out of a dire need to right the wrongs perpetrated against our society for the pursuit of profit and power.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
